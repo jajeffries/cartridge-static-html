863d7f959fc6b658cf4aafb1e904f35f93a88e09
Barney Scott <barney.scott@codecomputerlove.com>
2016-06-06 15:52:50 +0100

    test(task): Add in more functional testing to the project Start testing that the gulp task is generating the files we expect by adding the gulp runner framework

--- task.js
+++ task.js
@@ -12,8 +12,6 @@ var merge  = require('merge');
 //.Handlebars tasks
 var handlebars  = require('gulp-compile-handlebars');
 
-/* jshint node: true */
-
 // Basic data source api pending it being split out to another file
 function getDataSource(projectConfig) {
 	var api = {};
@@ -41,7 +39,7 @@ function getDataSource(projectConfig) {
 
 // Basic default data source pending it being split out
 function DefaultData(projectConfig) {
-	var defaultData = require(path.resolve(projectConfig.paths.src.views.data, '_default.json'));
+	var defaultData = require(path.resolve(projectConfig.paths.src.defaultdata, '_default.json'));
 
 	return {
 		apply: function apply(data) {
